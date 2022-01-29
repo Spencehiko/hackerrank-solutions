1ef7fd75f8a8b3bd949e6b4725328133d75caa7a
Spencehiko <hikmet.kufteoglu@gmail.com>
2022-01-29 18:18:35 +0300

    additional challenges

--- Data Structures/Easy/arrays-ds.js
+++ Data Structures/Easy/arrays-ds.js
@@ -1,7 +1,7 @@
 function reverseArray(a) {
     let reverseArr = [];
-    for(let i = 0; i < a.length; i++){ // a =  1,4,2,3
-        reverseArr[(a.length - i - 1)] = a[i]; // a[0] ----> a[3]
+    for(let i = 0; i < a.length; i++){ 
+        reverseArr[(a.length - i - 1)] = a[i]; 
     }
     return reverseArr;
 }
